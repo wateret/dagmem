90e9e76981776996616e407f48d9599d77d37f94
Hanjoung Lee <hanjoung.lee@samsung.com>
2019-12-02 18:26:32 +0900

    Update

--- src/solve.cc
+++ src/solve.cc
@@ -4,7 +4,6 @@ vector<int> solve(const vector<Node*>& nodes)
 {
   // This is a sample solution.
   // Erase the code and put your solution here.
-  return {0,1,3,2,4,5};
   vector<int> ret;
   for (int i = 0; i < nodes.size(); i++)
     ret.push_back(i);
